f21c447b812b48a49a0ae5a6dfb54263f0242d75
chris <160649430+chris-period@users.noreply.github.com>
2025-02-17 00:27:53 -0800

    Update frictionless-token.js

--- frictionless-token.js
+++ frictionless-token.js
@@ -1,240 +1,4 @@
-// reversing this token shit
-
-const V = [
-    "`9RY+%_J~]&2F",
-    "%z(EN8wC",
-    "`9RYCwTC",
-    "@h$N6/TC",
-    ")G>@@c{_4X",
-    "length",
-    "undefined",
-    63,
-    "call",
-    0,
-    18,
-    1,
-    4,
-    "NzfmiCG",
-    170,
-    137,
-    16,
-    3,
-    5,
-    "o2_CVt",
-    2,
-    9,
-    "yHSSBfZ",
-    "bqiR0NU",
-    7,
-    "gqi9pf",
-    127,
-    123,
-    "m3crkl",
-    8,
-    94,
-    6,
-    8191,
-    255,
-    202,
-    "XdGO3xH",
-    118,
-    119,
-    91,
-    13,
-    "TlLFED",
-    109,
-    88,
-    14,
-    "UbnV6CF",
-    15,
-    113,
-    89,
-    90,
-    333,
-    null,
-    "un95IQ5",
-    "apply",
-    null,
-    226,
-    21,
-    24,
-    22,
-    27,
-    30,
-    "kxl7rwS",
-    12,
-    "VvbsVE4",
-    115,
-    "duudpv",
-    "WxSaSiF",
-    33,
-    "JCObvQv",
-    "AKFMibb",
-    "YFNtbMc",
-    "CoHJRL",
-    48,
-    237,
-    66,
-    "SWm_lie",
-    29,
-    31,
-    101,
-    "fY5YMfr",
-    81,
-    522,
-    102,
-    "JoV5lz",
-    "VxONe2f",
-    59,
-    60,
-    "ylqSiq",
-    "LQUs2oB",
-    "DeZKRQ",
-    "v7TauZP",
-    "XniSOp",
-    43,
-    272,
-    122,
-    795,
-    61,
-    131,
-    132,
-    "G",
-    80,
-    104,
-    86,
-    71,
-    "e",
-    365,
-    "c",
-    52,
-    93,
-    128,
-    55,
-    44,
-    "M",
-    "d",
-    23,
-    53,
-    "O",
-    181,
-    "z",
-    "V",
-    "b",
-    149,
-    264,
-    11,
-    17,
-    251,
-    "AJ41Mfz",
-    "s",
-    "i",
-    "x",
-    "t",
-    "u",
-    "k",
-    "B",
-    244,
-    148,
-    76,
-    56,
-    "A",
-    "j",
-    895,
-    75,
-    "m",
-    46,
-    152,
-    154,
-    "f",
-    "y",
-    283,
-    "D",
-    158,
-    133,
-    58,
-    "n",
-    85,
-    "J",
-    "ty",
-    "HJAUlg",
-    121,
-    87,
-    "XGSTaG7",
-    35,
-    "A8HvK3",
-    175,
-    26,
-    97,
-    50,
-    57,
-    183,
-    "S",
-    192,
-    205,
-    209,
-    212,
-    45,
-    151,
-    238,
-    321,
-    "VlRz_H",
-    "kWJ3xCv",
-    37,
-    "pKqEP_o",
-    "z6_cwu",
-    "m16RE9W",
-    "Li6a4v",
-    106,
-    218,
-    194,
-    false,
-    240,
-    134,
-    "w",
-    98,
-    78,
-    227,
-    120,
-    "h",
-    "Lkd6XP",
-    "r",
-    421,
-    "tGsbWj",
-    "qXfTGuN",
-    130,
-    "k4a39xw",
-    "Jlk88Q",
-    172,
-    84,
-    394,
-    491,
-    "eYvt52",
-    "xkycbgW",
-    126,
-    302,
-    310,
-    319,
-    1000,
-    340,
-    100,
-    198,
-    346,
-    351,
-    355,
-    165,
-    998,
-    111,
-    "l",
-    369,
-    304,
-    370,
-    107,
-    "TQD75x"
-]
-
 // reversing how to generate ("token") key for /frictionless endpoint
-// const e = await Jx();
 
 function l(x, y, a) {
     if (a === 63)
@@ -249,9 +13,8 @@ function l(x, y, a) {
         return !x;
     if (a == -44)
         return x + y;
-    if (a == 61) {
+    if (a == 61)
         return void a;
-    }
     if (a == 68)
         return typeof a;
     if (a == -28)
@@ -260,55 +23,14 @@ function l(x, y, a) {
 }
 
 const a = l(Math.random(), 0.3, 63);
-const e = [
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0
-]
-const c = Math.min(l(e.reduce((i, r) => i + r, 0), a, -44), 1);
-console.log(a, c)
+const c = Math.min(l(0, a, -44), 1);
 
-function G(e, a = 0) {
-    var c = function () {
-        return e(...arguments);
-    };
-    return Object.defineProperty(c, "length", { value: a, configurable: !0 });
-}
-
-function N(e, a) {
-    var c = G((i) => V[i > -98 ? i + 97 : i + 12], 1);
-    return Object.defineProperty(e, c(-92), { value: a, configurable: !0 }), e;
-}
-
-
-const Ax = N((...e) => {
-    e.length = 1
-    e[66] = -126
-    e[1] = Date.now();
-
-
-    e[2] = l((Number(("" + e[1]).slice(l(3, 0, 24))) + e[0]) / (e[66] + 1125) * Math.PI, Math.PI / 2, -28);
-    e[3] = l(Math.sin(e[2]), 1000, 63);
-    return e[66] > -7 ? e[115] : [e[1], e[3]]
-});
+const Ax = (...e) => {
+    const currTime = Date.now();
+    const pie = l((Number(currTime.toString().slice(-3)) + e[0]) / (999) * Math.PI, Math.PI / 2, -28);
+    const sinner = l(Math.sin(pie), 1000, 63);
+    return [currTime, sinner]
+};
 
 async function encryptPlainText(...e) {
     const publicKeyBytes = atob(`MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoa1QCkVvFAfv+fgFpjfq
@@ -339,5 +61,4 @@ async function encryptPlainText(...e) {
 }
 
 const token = await encryptPlainText(JSON.stringify(Ax(c)));
-// console.log(Ax(c));
 console.log(token)
